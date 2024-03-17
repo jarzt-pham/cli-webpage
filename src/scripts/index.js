572580960b51e845187e0e245f2817cbba32d6ab
Jarzt <dev.luanpham@gmail.com>
2024-03-18 00:13:27 +0700

    feat: initial

---
+++ src/scripts/index.js
@@ -0,0 +1,34 @@
+help = [
+  `whois                  Who is Jarzt? Oh, it's me, but who is me?`,
+  `projects               View coding projects`,
+  `social                 Display social networks`,
+  `secret`,
+];
+
+function command(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      help;
+      break;
+
+    case "whois":
+      help;
+      break;
+
+    case "projects":
+      help;
+      break;
+
+    case "projects":
+      help;
+      break;
+  }
+}
+
+function help() {}
+
+function help() {}
+
+function help() {}
+
+function help() {}
